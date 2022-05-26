c638372b4b805726756c367016c2a429eac6cecd
Rabbeg <51306634+RabbegHydra@users.noreply.github.com>
2022-05-26 11:58:21 +0100

    Add files via upload

---
+++ video-list.js
@@ -0,0 +1,48 @@
+let allVideos = [
+   {
+      name: "Computer Monitor Shape",
+      src: "vid-1",
+      id: "vid_1"
+   },
+   {
+      name: "Button Hover Effect",
+      src: "vid-2",
+      id: "vid_2"
+   },
+   {
+      name: "Confirm Password using Html CSS & js",
+      src: "vid-3",
+      id: "vid_3"
+   },
+   {
+      name: "Creative Card Hover Effect",
+      src: "vid-4",
+      id: "vid_4"
+   },
+   {
+      name: "Creative Digital Clock Ui Design",
+      src: "vid-5",
+      id: "vid_5"
+   },
+   {
+      name: "Creative Our Team Section",
+      src: "vid-6",
+      id: "vid_6"
+   },
+   {
+      name: "Filter Text Animation Using Html and CSS",
+      src: "vid-7",
+      id: "vid_7"
+   },
+   {
+      name: "Glassmorphism Calculater New Design",
+      src: "vid-8",
+      id: "vid_8"
+   },
+   {
+      name: "How To  Make Cheatsheet",
+      src: "vid-9",
+      id: "vid_9"
+   }
+]
+
